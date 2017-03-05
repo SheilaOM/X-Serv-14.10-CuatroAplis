f975af06e1a1da4b0e72ccf24b4e0f4b475e5dd6
SheilaOM <sheila.oliva14@gmail.com>
2017-03-05 12:24:12 +0100

    primera version

---
+++ aleat.py
@@ -0,0 +1,15 @@
+import webapp
+import random
+
+class aleat(webapp.app):
+
+    def process (self, parsedRequest):
+        num = random.randint(0,100000000000)
+        num = "/aleat/" + str(num)
+        return ("200 OK", "<html><body><h1>" +
+                          "<a href=" + num + ">Dame mas</a>" +
+                          "</h1></body></html>")
+
+if __name__ == "__main__":
+    aleatApp = aleat()
+    testAleat = webapp.webApp("localhost", 1234, {'/aleat': aleatApp})
