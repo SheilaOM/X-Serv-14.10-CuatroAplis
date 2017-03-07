11a23d88caa7e2bc75780a5e16bd14e565c70734
SheilaOM <sheila.oliva14@gmail.com>
2017-03-07 15:30:02 +0100

    Corregido tras check

--- hola.py
+++ hola.py
@@ -1,16 +1,16 @@
 import webapp
 
 class hola(webapp.app):
-    def process (self, parsedRequest):
-        def process (self, parsedRequest):
-            return ("200 OK", "<html><body><h1>Hola</h1></body></html>")
+    def process(self, parsedRequest):
+        return ("200 OK", "<html><body><h1>Hola</h1></body></html>")
+
 
 class adios(webapp.app):
-    def process (self, parsedRequest):
+    def process(self, parsedRequest):
         return ("200 OK", "<html><body><h1>Adios</h1></body></html>")
 
 if __name__ == "__main__":
     holaApp = hola()
     adiosApp = adios()
     testHola = webapp.webApp("localhost", 1234, {'/hola': holaApp,
-                                                '/adios': adiosApp})
+                                                 '/adios': adiosApp})
